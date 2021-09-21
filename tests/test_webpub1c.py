24465acea5e3d8dc565df39be4839971f497df35
Dmitry <b4tm4n@mail.ru>
2021-09-21 10:01:51 +0300

    fix urlpath_join test

--- tests/test_webpub1c.py
+++ tests/test_webpub1c.py
@@ -24,6 +24,7 @@ def test_slugify(input_str: str, expected_str: str):
     ('/base', '/path', '/base/path'),
     ('/base/', 'path', '/base/path'),
     ('/base/', '/path', '/base/path'),
+    ('/base/', '/base/path', '/base/path'),
 ])
 def test_urlpath_join(prefix: str, url_path: str, expected_str: str):
     assert urlpath_join(prefix, url_path) == expected_str
