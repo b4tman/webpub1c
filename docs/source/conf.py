7abaf870b3dc4da12d2b7039b5218035dfb92ff5
Dmitry Belyaev <b4tman@ya.ru>
2023-08-19 18:03:48 +0300

    Apply suggestions from code review
    format with black
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'webpub1c'
-copyright = '2023, b4tman'
-author = 'b4tman'
+project = "webpub1c"
+copyright = "2023, b4tman"
+author = "b4tman"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser","sphinx.ext.autodoc", "sphinx_design", "sphinx.ext.viewcode", "sphinx_copybutton", "sphinx.ext.githubpages"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx_design",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx.ext.githubpages",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
